3b684cdd515bf30a08d246bda6705df24bc7780c
eduardobd2019 <49658345+eduardobd2019@users.noreply.github.com>
2019-06-24 20:04:54 -0300

    Update listagem.php

--- listagem.php
+++ listagem.php
@@ -7,6 +7,8 @@ $dados = [];
 //CADA REPETIÇÃO BUSCARA UM ITEM DO BANCO Enquanto tiver dados
 while ($linha = $smtp->fetch(PDO::FETCH_OBJ)) {
 	$dados[] = $linha;
+	
+	
 }
 echo json_encode($dados);   // Já converte para o formato JSON usado no Aplicativo
 ?>
